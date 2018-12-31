e902850584e225784ff6a73cceb414f8ed9157e3
Jorge Branquinho <Jorge.Branquinho@dominos.co.uk>
2018-12-31 15:07:12 +0000

    few changes

---
+++ LocalCommander/LocalCommander/Local/ControlledMachine.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace LocalCommander.Local
+{
+    public class ControlledMachine
+    {
+        public string MachineName => System.Environment.MachineName;
+
+        public bool IsOnline => System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+
+        public string IpAddress => GetLocalIpAddress();
+
+        private static string GetLocalIpAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+
+            return "0.0.0.0";
+        }
+    }
+}
---
+++ LocalCommander/LocalCommander/Local/ControlledProcess.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace LocalCommander.Local
+{
+    public class ControlledProcess
+    {
+        public bool IsInvalidOrEmpty { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ControlledProcess()
+        {
+        }
+
+        public ControlledProcess(Process process)
+        {
+            Id = process.Id;
+            Name = process.ProcessName;
+        }
+
+        public static ControlledProcess Empty()
+        {
+            var result = new ControlledProcess();
+            result.IsInvalidOrEmpty = true;
+            return result;
+        }
+    }
+}
